720933757ee667d796e63bf59fe95701d27d9673
javafa <nigne2016@gmail.com>
2017-05-23 15:54:30 +0900

    added Custom List

--- app/src/main/java/com/veryworks/android/adapterbasic/CustomListActivity.java
+++ app/src/main/java/com/veryworks/android/adapterbasic/CustomListActivity.java
@@ -3,21 +3,23 @@ package com.veryworks.android.adapterbasic;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class CustomListActivity extends AppCompatActivity {
 
     ListView listView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_custom_list);
+        listView = (ListView) findViewById(R.id.listView);
 
         // 1. 데이터
         ArrayList<Data> datas = Loader.getData();
@@ -27,38 +29,49 @@ public class CustomListActivity extends AppCompatActivity {
 
         // 3. 연결
         listView.setAdapter(adapter);
-
     }
 }
 
 class CustomAdapter extends BaseAdapter {
     ArrayList<Data> datas;
     Context context;
+    LayoutInflater inflater;
 
     public CustomAdapter(ArrayList<Data> datas, Context context) {
         this.datas = datas;
         this.context = context;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
     @Override
     public int getCount() { // 사용하는 데이터의 총 개수를 리턴...
-        return 0;
+        return datas.size();
     }
 
     @Override
     public Object getItem(int position) { // 데이터 클래스 하나를 리턴
-        return null;
+        return datas.get(position);
     }
 
     @Override
     public long getItemId(int position) { // 대부분 인덱스가 그대로 리턴된다
-        return 0;
+        return position;
     }
 
     // 아이템 뷰 하나를 리턴한다.
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        // xml 을 class 로 변환한다.
+        View view = inflater.inflate(R.layout.item_custom_list, null);
+        TextView no = (TextView) view.findViewById(R.id.txtNo);
+        TextView title = (TextView) view.findViewById(R.id.txtTitle);
+
+        // 매줄에 해당되는 데이터를 꺼낸다
+        Data data = datas.get(position);
+        no.setText(data.getId()+"");
+        title.setText(data.getTitle());
+
+        return view;
     }
 }
 
