a39aafde3df0f668ddd8cd5c53cd7182911e9f94
javafa <nigne2016@gmail.com>
2017-05-23 15:29:09 +0900

    added Custom List

--- app/src/main/java/com/veryworks/android/adapterbasic/MainActivity.java
+++ app/src/main/java/com/veryworks/android/adapterbasic/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
 
     Spinner spinner;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
