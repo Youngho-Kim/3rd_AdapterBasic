17a8daf02c0a5e1ac0e9b2ef5460c278c62e7234
javafa <nigne2016@gmail.com>
2017-05-24 13:00:42 +0900

    added Custom List

--- app/src/main/java/com/veryworks/android/adapterbasic/GridActivity.java
+++ app/src/main/java/com/veryworks/android/adapterbasic/GridActivity.java
@@ -1,7 +1,10 @@
 package com.veryworks.android.adapterbasic;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class GridActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class GridActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid);
+
+        // 1 데이터
+        ArrayList<Data> datas = new ArrayList<>();
+        datas = Loader.getData();
+        // 2 아답터
+        CustomAdapter adapter = new CustomAdapter(datas, this);
+
+        // 3 연결
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+        gridView.setAdapter(adapter);
     }
 }
